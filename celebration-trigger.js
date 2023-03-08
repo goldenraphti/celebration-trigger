91d1a4b0ad35a2dba63fe5de74f1927b21e01d3d
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2023-03-08 10:25:15 +0100

    export CelebrationTrigger

--- celebration-trigger.js
+++ celebration-trigger.js
@@ -1,3 +1,5 @@
 import { CelebrationTrigger } from './src/CelebrationTrigger.js';
 
+export { CelebrationTrigger } from './src/CelebrationTrigger.js';
+
 window.customElements.define('celebration-trigger', CelebrationTrigger);
